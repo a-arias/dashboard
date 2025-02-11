105984f047dabce029717954e0fa1ec2ed6cf88d
a-arias <abraham.arias@suse.com>
2025-02-10 23:16:23 -0700

    refactoring test spec file

--- cypress/e2e/tests/pages/explorer/dashboard/websockets/connection.spec.ts
+++ cypress/e2e/tests/pages/explorer/dashboard/websockets/connection.spec.ts
@@ -1,31 +1,35 @@
-describe('Connection with Websockets', { tags: ['@setup'] }, () => {
-  const CATTLE_TEST_URL = '';
-  const NAMESPACE = '';
-  const POD_NAME = '';
-  const CONTAINER_NAME = '';
-  const firstCommand = 'mkdir test-directory && echo "Directory created successfully"';
-  const secondCommand = 'ls | sed \'s/\x1b\\[([0-9,A-Z]{1,2}(;[0-9,A-Z]{1,2})?(;[0-9,A-Z]{3})?)?[m|K]//g\'\n';
-  const thirdCommand = 'rm -rf test-directory6 && echo "Directory deleted successfully"';
-  const BEARER_TOKEN = '';
+function executeWebSocket(command: string) {
+  const websocketUrl = 'wss://aarias.qa.rancher.space';
+  const namespace = '';
+  const podName = '';
+  const containerName = '';
+  const bearerToken = '';
 
+  return cy.setupWebSocket(
+    websocketUrl,
+    namespace,
+    podName,
+    containerName,
+    command,
+    bearerToken
+  );
+}
+
+describe('Connection with Websockets', { tags: ['@setup'] }, () => {
   it('should create a new folder', () => {
-    cy.setupWebSocket(CATTLE_TEST_URL, NAMESPACE, POD_NAME, CONTAINER_NAME, firstCommand, BEARER_TOKEN).then((messages) => {
-      cy.log('Received messages:', messages);
-      // ##Asserts the create new directory command was successful.
+    executeWebSocket('mkdir test-directory && echo "Directory created successfully"').then((messages) => {
       expect(messages[2]).to.include('Directory created successfully');
     });
   });
+
   it('should validate the folder name', () => {
-    cy.setupWebSocket(CATTLE_TEST_URL, NAMESPACE, POD_NAME, CONTAINER_NAME, secondCommand, BEARER_TOKEN).then((messages) => {
-      cy.log('Received messages:', messages);
-      // ##Asserts that the folder created on test one exists.
+    executeWebSocket('ls').then((messages) => {
       expect(messages[2]).to.include('test-directory');
     });
   });
+
   it('should delete the folder', () => {
-    cy.setupWebSocket(CATTLE_TEST_URL, NAMESPACE, POD_NAME, CONTAINER_NAME, thirdCommand, BEARER_TOKEN).then((messages) => {
-      cy.log('Received messages:', messages);
-      // ##Asserts the delete command was successful.
+    executeWebSocket('rm -rf test-directory && echo "Directory deleted successfully"').then((messages) => {
       expect(messages[2]).to.include('Directory deleted successfully');
     });
   });
