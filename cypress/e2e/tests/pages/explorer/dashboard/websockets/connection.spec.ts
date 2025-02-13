d251abb4730e9fcc697c42816a1c2be6489a7e4f
a-arias <abraham.arias@suse.com>
2025-02-12 17:53:50 -0700

    Adding create pod logic and refactoring the code of the function.

--- cypress/e2e/tests/pages/explorer/dashboard/websockets/connection.spec.ts
+++ cypress/e2e/tests/pages/explorer/dashboard/websockets/connection.spec.ts
@@ -1,9 +1,6 @@
-function executeWebSocket(command: string) {
-  const websocketUrl = 'wss://aarias211.qa.rancher.space';
- //   'wss://aarias.qa.rancher.space';
-  const namespace = 'default';
-  const podName = 'pod3';
-  const containerName = 'container-0';
+function executeWebSocket(command: string, podName: string, namespace: string, containerName: string) {
+  const websocketUrl = Cypress.env('api').replace('https', 'wss');
+  const bearerToken = '';
 
   return cy.setupWebSocket(
     websocketUrl,
@@ -15,21 +12,53 @@ function executeWebSocket(command: string) {
   );
 }
 
-describe('Connection with Websockets', { tags: ['@setup'] }, () => {
+describe('Create node and pod', { tags: ['@adminUser', '@standardUser', '@explorer'] }, () => {
+  before(() => {
+    cy.login();
+  });
+  let podName = `e2e-test`;
+  const projName = `project${ +new Date() }`;
+  const nsName = `namespace${ +new Date() }`;
+
+  it('should create a new pod', () => {
+    // get user id
+    cy.getRancherResource('v3', 'users?me=true').then((resp: Cypress.Response<any>) => {
+      const userId = resp.body.data[0].id.trim();
+
+      // create project
+      cy.createProject(projName, 'local', userId).then((resp: Cypress.Response<any>) => {
+        cy.wrap(resp.body.id.trim()).as('projId');
+
+        // create ns
+        cy.get<string>('@projId').then((projId) => {
+          cy.createNamespaceInProject(nsName, projId);
+        });
+
+        // create pod
+        // eslint-disable-next-line no-return-assign
+        cy.createPod(nsName, podName, 'nginx:stable-alpine3.20-perl').then((resp) => {
+          podName = resp.body.metadata.name;
+          // eslint-disable-next-line
+          cy.wait(2000);
+        });
+      });
+    });
+  });
+
   it('should create a new folder', () => {
-    executeWebSocket('mkdir test-directory && echo "Directory created successfully"').then((messages) => {
+    executeWebSocket('mkdir test-directory && echo "Directory created successfully"', podName, nsName, 'container-0').then((messages) => {
       expect(messages[2]).to.include('Directory created successfully');
     });
   });
 
   it('should validate the folder name', () => {
-    executeWebSocket('ls').then((messages) => {
+    executeWebSocket('ls', podName, nsName, 'container-0').then((messages) => {
       expect(messages[2]).to.include('test-directory');
     });
   });
 
   it('should delete the folder', () => {
-    executeWebSocket('rm -rf test-directory && echo "Directory deleted successfully"').then((messages) => {
+    executeWebSocket('rm -rf test-directory && echo "Directory deleted successfully"', podName, nsName, 'container-0').then((messages) => {
       expect(messages[2]).to.include('Directory deleted successfully');
     });
   });
