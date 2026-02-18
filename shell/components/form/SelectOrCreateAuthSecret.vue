4aa2c3f0445a38970645d6318328ba292884a171
Marcelo Fukumoto <marcelo.fukumoto@suse.com>
2026-02-18 18:05:49 +0100

    [AppCo] Remove the changes for the Select

--- shell/components/form/SelectOrCreateAuthSecret.vue
+++ shell/components/form/SelectOrCreateAuthSecret.vue
@@ -150,10 +150,6 @@ export default {
       default: false,
     },
 
-    fixedHttpBasicAuth: {
-      type:    Boolean,
-      default: false,
-    },
     isGithubDotComRepository: {
       type:    Boolean,
       default: false,
@@ -203,8 +199,6 @@ export default {
       filteredSecrets: null,
 
       selected: null,
-
-      previousValue: null,
 
       filterByNamespace: this.namespace && this.limitToNamespace,
 
@@ -281,10 +275,6 @@ export default {
           value: id,
         };
       });
-
-      if (this.fixedHttpBasicAuth) {
-        out = out.filter((o) => o.label.search('clusterrepo-appco-auth-') === 0 || ['title', 'divider'].includes(o.kind) || o.value === AUTH_TYPE._BASIC);
-      }
 
       if (this.allowS3) {
         const more = this.allCloudCreds
@@ -376,10 +366,6 @@ export default {
         });
       }
 
-      if (this.fixedHttpBasicAuth) {
-        out = out.filter((o) => o.label.search('clusterrepo-appco-auth-') === 0 || ['title', 'divider'].includes(o.kind) || o.value === AUTH_TYPE._BASIC);
-      }
-
       return out;
     },
 
@@ -405,17 +391,6 @@ export default {
   },
 
   watch: {
-    fixedHttpBasicAuth: {
-      immediate: true,
-      handler(newVal) {
-        if (newVal) {
-          this.previousValue = this.selected;
-          this.selected = AUTH_TYPE._BASIC;
-        } else {
-          this.selected = this.previousValue || AUTH_TYPE._NONE;
-        }
-      }
-    },
     selected:      'update',
     publicKey:     'updateKeyVal',
     privateKey:    'updateKeyVal',
@@ -448,7 +423,7 @@ export default {
 
   methods: {
     updateSelectedFromValue() {
-      let selected = this.preSelect?.selected || (this.fixedHttpBasicAuth ? AUTH_TYPE._BASIC : AUTH_TYPE._NONE);
+      let selected = this.preSelect?.selected || AUTH_TYPE._NONE;
 
       if ( this.value ) {
         if ( typeof this.value === 'object' ) {
